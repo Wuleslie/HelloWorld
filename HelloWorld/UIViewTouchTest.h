78ef5d3aa45eb9baf31ff32149b8f3db62fb4944
XueSong <sss@SSSdeMac-mini.local>
2018-06-14 11:12:43 +0800

    upload UIViewTouchTest

---
+++ HelloWorld/UIViewTouchTest.h
@@ -0,0 +1,13 @@
+//
+//  UIViewTouchTest.h
+//  HelloWorld
+//
+//  Created by XueSong on 2018/6/14.
+//  Copyright © 2018 raystudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewTouchTest : UIView
+
+@end
---
+++ HelloWorld/UIViewTouchTest.m
@@ -0,0 +1,21 @@
+//
+//  UIViewTouchTest.m
+//  HelloWorld
+//
+//  Created by XueSong on 2018/6/14.
+//  Copyright © 2018 raystudio. All rights reserved.
+//
+
+#import "UIViewTouchTest.h"
+
+@implementation UIViewTouchTest
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
